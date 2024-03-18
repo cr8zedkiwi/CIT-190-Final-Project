da53e6e181126dea4d9478203ff7c27076478698
Cameron <153962889+cr8zedkiwi@users.noreply.github.com>
2024-03-17 20:36:46 -0400

    Add files via upload

---
+++ TermProject/js/TermProjectJS.js
@@ -0,0 +1,15 @@
+document.getElementById("home").addEventListener("mouseover",function(){
+    document.getElementById("message").innerHTML="Click to select a game, hover over to see a description of the game.";
+})
+document.getElementById("melon").addEventListener("mouseover",function(){
+    document.getElementById("message").innerHTML="Try and get watermelon safely across the street! <br> <br> Cross as many streets as you can before you get squashed!";
+})
+document.getElementById("blueras").addEventListener("mouseover",function(){
+    document.getElementById("message").innerHTML="Time smash, try and get the new highscore! <br> <br> Can you beat Bluerasberry's highscore in this reaction speed based game?";
+})    
+document.getElementById("grape").addEventListener("mouseover",function(){
+    document.getElementById("message").innerHTML="Classical snake but jolly rancher themed! <br> <br> Come give this game a try and see how long you can survive!";
+}) 
+document.getElementById("readme").addEventListener("mouseover",function(){
+    document.getElementById("message").innerHTML="Includes game descriptions, contact information and copyright information.";
+}) 
---
+++ TermProject/readme/readme.txt
@@ -0,0 +1,29 @@
+|GAME DESCRIPTIONS|
+
+1. Try and get watermelon safely across the street! <br> <br> Cross as many streets as you can before you get squashed!
+
+2. Time smash, try and get the new highscore! <br> <br> Can you beat Bluerasberry's highscore in this reaction speed based game?
+
+3. Classical snake but jolly rancher themed! <br> <br> Come give this game a try and see how long you can survive!
+
+
+
+------------------------------------------------
+
+
+
+|CONTACT INFORMATION|
+
+Author: Cameron Lawson
+Email: lawson69@mail.nmc.edu
+
+
+
+------------------------------------------------
+
+
+
+|COPYRIGHT INFORMATION|
+
+If a .html file is reproduced, please provide credits to the author in the page interface.
+If a .js or .css file is reproduced, please provide credits to the author in the comments or documentation.
